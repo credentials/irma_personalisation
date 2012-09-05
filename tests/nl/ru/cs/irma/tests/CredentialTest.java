17de94e523028761c0a4c80bdb05ac1cb0be2a2e
Wouter Lueks <wouter@telox.net>
2012-09-05 13:21:54 +0200

    Updated IrmaIssuer to work with new Surfnet Root credential
    Please make sure to link/copy irma_configuration into the root of your checkout.

---
+++ tests/nl/ru/cs/irma/tests/CredentialTest.java
@@ -0,0 +1,31 @@
+package nl.ru.cs.irma.tests;
+
+import javax.smartcardio.CardException;
+
+import net.sourceforge.scuba.smartcards.CardServiceException;
+import nl.ru.cs.irma.irmawriter.IrmaIssuer;
+
+import org.junit.Test;
+
+import credentials.Attributes;
+import credentials.CredentialsException;
+
+public class CredentialTest {
+	@Test
+	public void credentialIssue() throws CardException, CredentialsException, CardServiceException {
+		IrmaIssuer ii = new IrmaIssuer();
+		
+		ii.issue(getSurfnetAttributes());
+	}
+	
+    private Attributes getSurfnetAttributes() {
+        // Return the attributes that have been revealed during the proof
+        Attributes attributes = new Attributes();
+
+		attributes.add("userID", "s654321@ru.nl".getBytes());
+		attributes.add("securityHash", "DEADBEEF".getBytes()); // FIXME: Don't care for now
+		
+		return attributes;
+	}
+
+}
